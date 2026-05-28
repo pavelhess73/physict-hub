6720fc8d40ffbe37f9c0de584e2b13a99fcc9f3e
Pavel <78315934+pavelhess73@users.noreply.github.com>
2026-05-28 09:48:48 +0200

    Fix unused imports to resolve build errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { 
   Activity, 
   Cpu, 
-  Timer, 
   ExternalLink, 
   Settings, 
   Home, 
