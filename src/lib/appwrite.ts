6d35e8296cbc0f713d613bb7231747d15406f0c6
Pavel <78315934+pavelhess73@users.noreply.github.com>
2026-05-28 10:42:37 +0200

    Update Appwrite Project ID

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -2,7 +2,7 @@ import { Client, Databases, Account } from 'appwrite';
 
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1') // Appwrite Endpoint
-    .setProject('VASI_PROJECT_ID'); // ZDE DOPLNÍTE SVÉ PROJECT ID
+    .setProject('6a17fbd7120cc796e1ea'); // ZDE DOPLNÍTE SVÉ PROJECT ID
 
 export const databases = new Databases(client);
 export const account = new Account(client);
